400e718aaeae5167abb27c2d5a493730ec447494
Alexandre-JAMROZ <alexandrejamrozpro@gmail.com>
2025-08-26 20:14:19 +1100

    creer: nav.php (code html du nav) | modifier: remplacé code nav html par nav.php (alléger le code php des pages)| ajout: style header dans style.css

---
+++ config/nav.php
@@ -0,0 +1,14 @@
+<div class="nav-container">
+    <nav>
+        <ul>
+            <li><a href="/">Accueil</a></li>
+            <li><a href="documentation/documentation.html">Documentation</a></li>
+            <li><a href="modules/modules.html">Modules</a></li>
+            <li><a href="/compte.php">Mon compte</a></li>
+            <?php include __DIR__ . "/../config/logoutButton.php"; ?>
+        </ul>
+    </nav>
+    <div class="profile">
+        <img src="https://via.placeholder.com/40" alt="Photo de profil">
+    </div>
+</div>
